45b08319223b1237f0efcbba8637d3049952f229
Jameelaher <54056284+Jameelaher@users.noreply.github.com>
2019-08-15 16:09:51 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-/**
- Made By 旋之华(www.it666.com)
- */
+
 $(function () {
     $(window).on("resize", function () {
         // 1.1 获取窗口的宽度
@@ -72,11 +70,21 @@ $(function () {
     
     // 4. 导航处理
     let allLis = $("#lk_nav li");
-    
+    $(allLis[0]).on("click", function () {
+        $("html,body").animate({ scrollTop: $("").offset().top }, 1000);
+    });
+    $(allLis[1]).on("click", function () {
+        $("html,body").animate({ scrollTop: $("#lk_about").offset().top }, 1000);
+    });
     $(allLis[2]).on("click", function () {
+        $("html,body").animate({ scrollTop: $("#lk_product").offset().top }, 1000);
+    });
+    $(allLis[3]).on("click", function () {
         $("html,body").animate({ scrollTop: $("#lk_hot").offset().top }, 1000);
     });
-
-
+    $(allLis[4]).on("click", function () {
+        $("html,body").animate({ scrollTop: $("#lk_footer").offset().top }, 1000);
+    });
     $(window).trigger("resize");
+
 });
